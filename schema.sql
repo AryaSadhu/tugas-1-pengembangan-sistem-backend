ebc576f9db99a43078f8c0ac31824dd3161e3b4a
AryaSadhu <108924697+AryaSadhu@users.noreply.github.com>
2025-11-27 00:47:12 +0800

    Add schema.sql

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS products (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   category VARCHAR(50) NOT NULL,
-  price DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  price VARCHAR(50) NOT NULL,
   stock INT NOT NULL DEFAULT 0,
   image_path VARCHAR(255) DEFAULT NULL,
   status ENUM('active','inactive') NOT NULL DEFAULT 'active',
