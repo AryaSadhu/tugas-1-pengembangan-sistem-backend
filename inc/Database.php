f6e612df29edd3ac3e6632eb21813fadd147b86b
AryaSadhu <108924697+AryaSadhu@users.noreply.github.com>
2025-11-27 00:25:24 +0800

    Update Database.php connection settings

--- inc/Database.php
+++ inc/Database.php
@@ -9,14 +9,21 @@ class Database
 
     public static function getConnection(): \PDO
     {
-        if (self::$pdo === null) {
-            $options = [
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES => false,
-            ];
-            self::$pdo = new \PDO(getDsn(), DB_USER, DB_PASS, $options);
-        }
-        return self::$pdo;
+    if (self::$pdo === null) {
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        // --- FIX DI SINI ---
+        self::$pdo = new \PDO(
+            'mysql:host=localhost;dbname=tugas1_crud;charset=utf8',
+            DB_USER,
+            DB_PASS,
+            $options
+        );
     }
+    return self::$pdo;
+}
 }
